05beab0bf66e6a94524a3764bd1e61ffeb671803
Engoa <idobz1234567890@gmail.com>
2021-11-08 15:27:02 +0200

    typo fix

--- src/components/LogInSignup/LoginSignup.vue
+++ src/components/LogInSignup/LoginSignup.vue
@@ -212,7 +212,7 @@ export default {
           });
           this.setUserData(this.user);
           setTimeout(() => {
-            this.$router.replace("/");
+            location.reload();
           }, 2000);
         }
       }
